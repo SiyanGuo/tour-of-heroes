6dfb465751c109a5549fd07777b7e75d77d67798
Serena Guo <syg9345@gmail.com>
2022-04-01 23:37:02 -0400

    add HttpClient

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,14 +12,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
@@ -29,4 +22,17 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return;}
+    this.heroService.addHero( {name} as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    })
+  }
+
+  delete(hero:Hero):void {
+    this.heroes = this.heroes.filter(h=> h!==hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
